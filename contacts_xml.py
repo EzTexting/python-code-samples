e375f4a2837c31188f3f8d08f095dc9276ded8ca
Ez Texting <lyuba@eztexting.com>
2012-09-10 14:05:10 -0400

    work with contacts using xml/json

---
+++ contacts_xml.py
@@ -0,0 +1,29 @@
+import EzTextingXML as EzTexting
+
+ez = EzTexting.EzTextingXML("https://app.eztexting.com", "ezdemo", "password")
+
+
+try:
+    print 'get_all_contacts'
+    print ez.get_all_contacts(group='Honey Lovers')
+    print
+
+    contact = EzTexting.Contact('2123456796', 'Piglet', 'P.', 'piglet@small-animals-alliance.org', 'It is hard to be brave, when you are only a Very Small Animal.')
+    contact = ez.create_contact(contact)
+    print 'create_contact: ' + str(contact)
+
+    contact = ez.get_contact_by_id(contact.id)
+    print 'get_contact_by_id: ' + str(contact)
+
+    contact.groups=['Friends', 'Neighbors']
+    contact = ez.update_contact(contact)
+    print 'update_contact: ' + str(contact)
+
+    print 'delete.'
+    ez.delete_contact(contact.id)
+
+    print 'second delete. try to get error'
+    ez.delete_contact(contact.id)
+
+except EzTexting.EzTextingError, error:
+    print str(error)
